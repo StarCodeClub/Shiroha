98ccc05706a2f2ba9da4fa63e0d697d7700872b7
mrhua269 <mrhua269@gmail.com>
2025-10-19 10:05:13 +0800

    Fix up

---
+++ shiroha-server/src/main/java/i/moniasuki/shiroha/config/functions/LeavesModifyConfig.java
@@ -0,0 +1,13 @@
+package i.moniasuki.shiroha.config.functions;
+
+import me.earthme.luminol.config.flags.ConfigClassInfo;
+import me.earthme.luminol.config.flags.ConfigInfo;
+import me.earthme.luminol.enums.EnumConfigCategory;
+
+@ConfigClassInfo(name = "leaves_modify_config", category = EnumConfigCategory.FUNCTION)
+public class LeavesModifyConfig {
+    @ConfigInfo(name = "thrown_egg_or_snowball_can_knockback")
+    public static boolean thrownEggOrSnowballCanKnockback = false;
+    @ConfigInfo(name = "no_chat_sign")
+    public static boolean noChatSign = false;
+}
